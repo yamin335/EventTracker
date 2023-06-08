b201950d652e0fce0bceea3fb93d1e5b0ba48128
Yamin <myamin335@gmail.com>
2023-06-09 00:12:07 +0200

    Initial Commit

---
+++ EventTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EventTracker
+//
+//  Created by Md. Yamin on 09.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EventTracker/EventTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTrackerApp.swift
+//  EventTracker
+//
+//  Created by Md. Yamin on 09.06.23.
+//
+
+import SwiftUI
+
+@main
+struct EventTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
